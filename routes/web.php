e8ab4c0b3369d394aee22fb6dd089a4c6fe3c47e
AnjasFedo <fedoaf98@gmail.com>
2024-08-26 22:50:35 +0700

    02 Your First Route and View

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    // return 'About Laravel';
+    // return [
+    //     'name' => 'Laravel',
+    // ]; // json
+
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
